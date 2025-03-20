7ab11075c4065b5803c66cf3606dce98b914f0b1
Ioannis Ypsilos <ioypsilo@microsoft.com>
2025-03-20 13:54:37 +0200

    Added original publish files

---
+++ wwwroot/Functions.js
@@ -0,0 +1,13 @@
+﻿export function openLink(parent, url) {
+    window.location.href = url;
+    //alert('open link: ' + url);
+    /*var button = globalThis.document.createElement('button');
+    button.innerText = 'Hello world';
+    var clickCount = 0;
+    button.onclick = () => {
+        clickCount++;
+        button.innerText = 'Click count ' + clickCount;
+    };
+    parent.appendChild(button);
+    return button;*/
+}
